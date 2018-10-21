dfad331597cf4cb6f8990956a34068671455c27d
Muhammad Akma Adhwa <muhdakma63@gmail.com>
2018-10-21 16:54:05 +0800

    Updates

---
+++ precache-manifest.4a30e19ba14983ac258187553fe87995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "275cbf7599d5af2e6700",
+    "url": "/Github-User-Finder/static/js/runtime~main.275cbf75.js"
+  },
+  {
+    "revision": "03c58ad3e9f27ae35a07",
+    "url": "/Github-User-Finder/static/js/main.03c58ad3.chunk.js"
+  },
+  {
+    "revision": "4f3f319d42e94597808d",
+    "url": "/Github-User-Finder/static/js/1.4f3f319d.chunk.js"
+  },
+  {
+    "revision": "03c58ad3e9f27ae35a07",
+    "url": "/Github-User-Finder/static/css/main.788a89e9.chunk.css"
+  },
+  {
+    "revision": "4f3f319d42e94597808d",
+    "url": "/Github-User-Finder/static/css/1.bcd2503d.chunk.css"
+  },
+  {
+    "revision": "caae3497b7b368d28c0cf4c3875b30ca",
+    "url": "/Github-User-Finder/index.html"
+  }
+];
